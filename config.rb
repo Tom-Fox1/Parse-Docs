bd77a06ca833f7c08e1079dd12e0d413c600b365
Tom Fox <tomfox@surprises.io>
2019-03-31 16:46:15 +0100

    Update config.rb

--- config.rb
+++ config.rb
@@ -56,12 +56,3 @@ set :port, 4567
 helpers do
   require './lib/toc_data.rb'
 end
-
-activate :deploy do |deploy|
-  deploy.method = :git
-  # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-  # deploy.branch   = 'custom-branch' # default: gh-pages
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
-end
